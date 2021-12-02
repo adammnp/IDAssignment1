b50b70e0749e02a21e3a09c1bbd3bd14d9e9bc33
adammnp <94303754+adammnp@users.noreply.github.com>
2021-12-03 00:58:52 +0800

    Added form validation
    What's new?
    - Made it so that input is required or else form submit button is disabled.
    - Pop up added upon successful submission.

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-function()
+$('#swag input[required]').on('input propertychange paste change', function() {
+
+    var empty = $('#swag').find('input[required]').filter(function() {
+      return this.value == '';
+    });
+  
+    $('#register').prop('disabled', (empty.length))
+  
+  })
+  
